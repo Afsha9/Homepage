2781a9c413ef7d17bd67b1f2f84456368cfb707b
Afsha <113248081+Afsha9@users.noreply.github.com>
2022-09-17 14:24:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,26 @@ function getNumberOrString(value) {
 
 
 document.getElementById('button').addEventListener('click', (event) => {
+  let element_text = document.getElementById('text');
+  window.speechSynthesis.speak(new SpeechSynthesisUtterance(getNumberOrString(document.getElementById('text').value)));
+
+});
+
+
+
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
+}
+
+
+
+document.getElementById('button').addEventListener('click', (event) => {
   let element_list = document.getElementById('list');
   let new_li = document.createElement('li');
   new_li.innerText = getNumberOrString(document.getElementById('text').value);
